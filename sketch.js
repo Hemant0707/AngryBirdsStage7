399ddd9b773e7efdb3e6645a01459916303a67df
ksparimala <72539701+ksparimala@users.noreply.github.com>
2021-03-08 15:42:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,13 +5,14 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1,pig3;
-var backgroundImg,platform;
+var backgroundImage,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    //backgroundImg = loadImage("sprites/bg.png");
+    getTime()
 }
 
 function setup(){
@@ -45,7 +46,8 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImage)
+    background(backgroundImage);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
@@ -85,4 +87,26 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+//async stands for asychronous function: it will wait until the another request completed
+async function getTime()
+{
+    var response=await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+    var responsejson=await response.json();
+    console.log(responsejson);
+    var dt=responsejson.datetime;
+    console.log(dt);
+    var hour=dt.slice(11,13);
+    console.log(hour);
+
+    if  (hour>6&&hour<18)
+    {
+       bg="sprites/bg.png"
+    }
+    else
+    {
+        bg="sprites/bg2.jpg"
+    }
+    backgroundImage=loadImage(bg)
+
 }
